a0150bd1c5ebe31283adc6100abd4aa5f6557579
Heba Mohamed <hm18120182@gmail.com>
2023-03-19 15:38:17 -0700

    mod

--- lab1/Models/Car.cs
+++ lab1/Models/Car.cs
@@ -13,24 +13,4 @@ public class Car
     [DateInPast]
     public DateTime ProductionDate { get; set; }
     public double Price { get; set; }
-
-    // public Car(int id, string model, DateTime productionDate, double price, string type)
-    // {
-    //     Id = id;
-    //     Model = model;
-    //     ProductionDate = productionDate;
-    //     Price = price;
-    //     Type = type;
-    // }
-
-    // private static List<Car> _cars = new List<Car>();
-    // {
-    //     new (1, "Samsung", new DateTime(2011, 5, 5), 799.99),
-    //     new (2, "Apple", new DateTime(2012, 5, 5), 999.99),
-    //     new (3, "OnePlus", new DateTime(2013, 5, 5), 699.99),
-    //     new (4, "Huawei", new DateTime(2014, 5, 5), 899.99),
-    //     new (5, "Nokia", new DateTime(2015, 5, 5), 499.99)
-    // };
-
-    // public static List<Car> GetCars() => _cars;
 }
